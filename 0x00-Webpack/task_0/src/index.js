6f8389c306b1dbcbe060286d6f7627b72c9e5025
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-05-13 16:10:32 +0300

    webpack

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,3 +1,5 @@
 const $ = require('jquery');
 
-$('body').append('')
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
