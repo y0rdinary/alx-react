7119eb1d0ee2972c3037468ab646d4f395cd162b
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-05-16 19:49:45 +0300

    task 2

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -3,9 +3,17 @@ import './Notifications.css'
 
 function Notifications() {
     return (
-        <div className= "Notifications">
-            <p>Here is the list of notifications</p>
-        </div>
+        <div className="Notifications">
+          <p style={{display:'inline',}}>Here is the list of notifications</p>
+          <ul>
+            <li data='default' style={{paddingLeft:'5px'}}>New course available</li>
+            <li data='urgent'style={{paddingLeft:'5px'}}>New resume available</li>
+            <li data='urgent' dangerouslySetInnerHTML={{__html:getLatestNotification()}}></li>
+          </ul>
+          <button aria-label='Close' style={{display: 'inline', float: 'right', margin:'-115px -5px 0px 0px', border: 'none', background:'white'}} onClick= {console.log('Close button has been clicked')}>
+            <img src={closeIcon} alt='close' width='10px'/>
+          </button>
+      </div>
     );
 }
 
