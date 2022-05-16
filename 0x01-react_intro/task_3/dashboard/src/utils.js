25b582ea03698c32c2f4b96303daf65778849727
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-05-16 20:02:47 +0300

    task 2

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,3 @@
+export const getFullYear = () => new Date().getFullYear;
+export const getFooterCopy = (isIndex) => isIndex ? 'Holberton School': 'Holberton Schoolmain dashboard';
+export const getLatestNotification = () => '<strong>Urgent requirement</strong> - complete by EOD';
