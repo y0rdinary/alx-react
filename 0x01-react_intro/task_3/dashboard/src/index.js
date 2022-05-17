3772b82a874b5054eb53b359c00b0f5d1d08421a
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-05-17 09:58:38 +0300

    task 3

--- 0x01-react_intro/task_3/dashboard/src/index.js
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import reportWebVitals from './Notifications';
 import Notifications from './Notifications';
 
