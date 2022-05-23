20cdb89be9eb3a3d7f9204a10ff28dc977631870
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-05-23 10:41:58 +0300

    modified

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -6,12 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>School Dashboard</h1>
-        </header>
-        <body className='App-body'>
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
         <p>Login to access the full dashboard</p>
-        </body>
-        <footer className='App-footer'>
+      </body>
+      <footer className='App-footer'>
         <p>Copyright {getFullYear()} - {getFooterCopy('true')}</p>
       </footer>
     </div>
--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -3,10 +3,10 @@ import './Notifications.css'
 
 function Notifications() {
     return (
-        <div className= "Notifications">
-            <p>Here is the list of notifications</p>
-        </div>
+      <div className="Notifications">
+          <p>Here is the list of notifications</p>
+      </div>
     );
-}
-
-export default Notifications;
+  }
+  
+  export default Notifications;
